5e0cf975ff5c0f1b4192d3d6872fe8849ac4be5b
CORENTIN DAVID <corentin.david@iut.local>
2025-09-19 10:27:46 +0200

    troisieme commit

--- LectureEcritureJava/src/Image.java
+++ LectureEcritureJava/src/Image.java
@@ -3,56 +3,66 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 public class Image {
-    private int width;
-    private int height;
-    // pixels[y][x][0=R,1=G,2=B]
-    private int[][][] pixels;
+	private int width;
+	private int height;
+	// pixels[y][x][0=R,1=G,2=B]
+	private int[][][] pixels;
 
-    public int getWidth() { return width; }
-    public int getHeight() { return height; }
+	public int getWidth() { return width; }
+	public int getHeight() { return height; }
 
-    /**
-     * Constructeur : initialise une image vide.
-     */
-    public Image(int width, int height) {
-        this.width = width;
-        this.height = height;
-        pixels = new int[height][width][3];
-    }
+	/**
+	 * Constructeur : initialise une image vide.
+	 */
+	public Image(int width, int height) {
+		this.width = width;
+		this.height = height;
+		pixels = new int[height][width][3];
+	}
 
-    /**
-     * Définit la couleur d'un pixel à la position (x, y)
-     */
-    public void setPixel(int x, int y, int r, int g, int b) {
-        if (x >= 0 && x < width && y >= 0 && y < height) {
-            pixels[y][x][0] = r;
-            pixels[y][x][1] = g;
-            pixels[y][x][2] = b;
-        }
-    }
+	/**
+	 * Définit la couleur d'un pixel à la position (x, y)
+	 */
+	public void setPixel(int x, int y, int r, int g, int b) {
+		if (x >= 0 && x < width && y >= 0 && y < height) {
+			pixels[y][x][0] = r;
+			pixels[y][x][1] = g;
+			pixels[y][x][2] = b;
+		}
+	}
 
-    /**
-     * Sauvegarde l'image au format texte PPM (P3)
-     */
-    public void save_txt(String filename) throws IOException {
-        try (FileWriter fw = new FileWriter(filename)) {
-            fw.write("P3\n");
-            fw.write(width + " " + height + "\n");
-            fw.write("255\n");
+	/**
+	 * Sauvegarde l'image au format texte PPM (P3)
+	 */
+	public void save_txt(String filename) throws IOException {
+		try (FileWriter fw = new FileWriter(filename)) {
+			fw.write("P3\n");
+			fw.write(width + " " + height + "\n");
+			fw.write("255\n");
 
-            for (int y = 0; y < height; y++) {
-                for (int x = 0; x < width; x++) {
-                    fw.write(pixels[y][x][0] + " " +
-                             pixels[y][x][1] + " " +
-                             pixels[y][x][2] + " ");
-                }
-                fw.write("\n");
-            }
-        }
-    }
-    
-    static public read_txt(String filename) throws IOException {
-       byte[]data = fs.readAllBytes();
-       String txt = new String(data,StandardCharsets.UTF_8);
-    }
+			for (int y = 0; y < height; y++) {
+				for (int x = 0; x < width; x++) {
+					fw.write(pixels[y][x][0] + " " +
+							pixels[y][x][1] + " " +
+							pixels[y][x][2] + " ");
+				}
+				fw.write("\n");
+			}
+		}
+	}
+
+	static public void read_txt(String filename) throws IOException {
+		byte[]data = fs.readAllBytes();
+		String txt = new String(data,StandardCharsets.UTF_8);
+		int nbtoken = 0;
+		for ()
+	}
+
+	public static Image read_bin(String filename) throws IOException {
+		
+	}
+
+	public void write_bin(String filename) throws IOException {
+		
+		}
 }
